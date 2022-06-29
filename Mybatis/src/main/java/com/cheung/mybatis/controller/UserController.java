426a11043058e3983cc8845819105f5c100ed80d
kin <kin@localhost>
2022-06-29 23:46:12 +0800

    bug fixed

--- Mybatis/src/main/java/com/cheung/mybatis/controller/UserController.java
+++ Mybatis/src/main/java/com/cheung/mybatis/controller/UserController.java
@@ -30,29 +30,6 @@ public class UserController {
 	@Autowired
 	private AddressRepository addressRepository;
 
-	@GetMapping("/create")
-	public String create(ModelMap map) {
-		map.addAttribute("user", new User());
-		return "createUser";
-	}
-
-	@PostMapping("/create")
-	public String save(@ModelAttribute("user") User user, @RequestParam("role") String role, ModelMap model) {
-		User resultName = userRepository.findByNameOrEmail(user.getUserName());
-		User resultEmail = userRepository.findByNameOrEmail(user.getEmail());
-		if (resultName == null && resultEmail == null) {
-			user.setRole(role);
-			BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
-			String encodedPassword = bcryptPasswordEncoder.encode(user.getPassword());
-			user.setPassword(encodedPassword);
-			userRepository.save(user);
-			return "redirect:/login";
-		} else {
-			model.addAttribute("err", "The user is existing!");
-			return "createUser";
-		}
-	}
-
 	@GetMapping("update/{userId}")
 	public String updatePage(@PathVariable("userId") Integer userId, ModelMap map) {
 		User user = userRepository.findById(userId);
